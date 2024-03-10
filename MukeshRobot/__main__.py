5d7af0c3f682b00115b877e501f77ca9a694ee59
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-03-10 14:39:42 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -221,7 +221,7 @@ def test(update: Update, context: CallbackContext):
     print(update.effective_message)
 
 
-run_async
+
 def start(update: Update, context: CallbackContext):
     args = context.args
     uptime = get_readable_time((time.time() - StartTime))
