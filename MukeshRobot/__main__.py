8478a42a56cbe0a552abe5c2a5d222b932f73a83
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-03-17 11:39:55 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -98,8 +98,8 @@ buttons = [
         ), 
     ], 
     [ 
-        InlineKeyboardButton(text=" ⍟ ʜᴇʟᴘ ⍟ ", callback_data="help_back"), 
-        InlineKeyboardButton(text=" ♪ Mᴜsɪᴄ ♪ ", callback_data="Music_"), 
+        InlineKeyboardButton(text=" 🪬 ʜᴇʟᴘ 🪬 ", callback_data="help_back"), 
+        InlineKeyboardButton(text=" 🎶 Mᴜsɪᴄ 🎶  ", callback_data="Music_"), 
     ],
     
     [ 
@@ -119,7 +119,7 @@ buttons = [
  
  
     [ 
-        InlineKeyboardButton(text="☯︎ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ 𝐃𝐫𝐢𝐟𝐭𝐞𝐫࿐●🏎️ ☯︎ ", callback_data="Radiux_"), 
+        InlineKeyboardButton(text="☢︎︎ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ 𝐃𝐫𝐢𝐟𝐭𝐞𝐫࿐●🏎️ ☢︎︎ ", callback_data="Radiux_"), 
     ], 
 ] 
 
