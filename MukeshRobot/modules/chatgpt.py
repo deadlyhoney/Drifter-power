f0f9a82759693d283221954e0995f4de8e9c4e33
tozi-drifter <154064345+tozi-drifter@users.noreply.github.com>
2024-03-20 02:03:15 +0530

    Update chatgpt.py

--- MukeshRobot/modules/chatgpt.py
+++ MukeshRobot/modules/chatgpt.py
@@ -1,25 +1,40 @@
-import requests
-from .. import pbot as Mukesh,BOT_NAME,BOT_USERNAME
+import random
 import time
+import requests
+from MukeshRohot import app
+from config import BOT_USERNAME
+
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
-@Mukesh.on_message(filters.command(["gpt","ai"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
-async def chat(bot, message):
-    
-    try:
-        start_time = time.time()
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-        if len(message.command) < 2:
-            await message.reply_text(
-            "❍ Example ➛ /gpt ᴡʜᴇʀᴇ ɪs ᴛᴀᴊᴍᴀʜᴀʟ ?")
-        else:
-            a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://mukesh-api.vercel.app/chatgpt/{a}') 
-            x=response.json()["results"]
-            end_time = time.time()
-            telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
-            await message.reply_text(f"❍ {x}\n\n❍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ ๛ᴀ ᴠ ɪ s ʜ ᴀ ༗ ", parse_mode=ParseMode.MARKDOWN)     
-    except Exception as e:
-        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
 
-__mod_name__ = "ᴀɪ-ɢᴘᴛ"
+@app.on_message(filters.command(["ai","ask"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
+async def chat_gpt(bot, message):
+    try:
+        start_time = time.time()
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+
+        if len(message.command) < 2:
+            await message.reply_text(
+                "✦ ᴇxᴀᴍᴘʟᴇ ➠\n\n✦ /ask  ᴡʜᴇʀᴇ ɪs ᴛᴀᴊᴍᴀʜᴀʟ ?"
+            )
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
+
+            try:
+                
+                if "answer" in response.json():
+                    x = response.json()["answer"]
+                    end_time = time.time()
+                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
+                    await message.reply_text(
+                        f"🪖 {x}  \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  @garouProXbot",
+                        parse_mode=ParseMode.MARKDOWN
+                    )
+                else:
+                    await message.reply_text("✦ ɴᴏ 'results' ᴋᴇʏ ғᴏᴜɴᴅ ɪɴ ᴛʜᴇ ʀᴇsᴘᴏɴsᴇ.")
+            except KeyError:
+                
+                await message.reply_text("✦ ᴇʀʀᴏʀ ᴀᴄᴄᴇssɪɴɢ ᴛʜᴇ ʀᴇsᴘᴏɴsᴇ.")
+    except Exception as e:
+        await message.reply_text(f"error : {e} ")
